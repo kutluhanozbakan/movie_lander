6e6e0329575861cf0c3b697ff2f322240b2efd38
Vahit Kutluhan ÖZBAKAN <kutluhan.ozbakan@ardgrup.com.tr>
2023-11-29 16:28:46 +0300

    location

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,127 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:timezone/data/latest.dart' as tz;
+import 'package:timezone/timezone.dart' as tz;
 
-import 'src/app.dart';
-import 'src/settings/settings_controller.dart';
-import 'src/settings/settings_service.dart';
+void main() {
+  runApp(MyApp());
+}
 
-void main() async {
-  // Set up the SettingsController, which will glue user settings to multiple
-  // Flutter Widgets.
-  final settingsController = SettingsController(SettingsService());
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Hatırlatıcı Notlar',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: NoteListScreen(),
+    );
+  }
+}
 
-  // Load the user's preferred theme while the splash screen is displayed.
-  // This prevents a sudden theme change when the app is first displayed.
-  await settingsController.loadSettings();
+class NoteListScreen extends StatefulWidget {
+  @override
+  _NoteListScreenState createState() => _NoteListScreenState();
+}
 
-  // Run the app and pass in the SettingsController. The app listens to the
-  // SettingsController for changes, then passes it further down to the
-  // SettingsView.
-  runApp(MyApp(settingsController: settingsController));
+class _NoteListScreenState extends State<NoteListScreen> {
+  final TextEditingController _noteController = TextEditingController();
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+
+  @override
+  void initState() {
+    super.initState();
+    initializeNotifications();
+  }
+
+  Future<void> initializeNotifications() async {
+    const AndroidInitializationSettings initializationSettingsAndroid =
+        AndroidInitializationSettings('app_icon');
+    final InitializationSettings initializationSettings =
+        InitializationSettings(android: initializationSettingsAndroid);
+    await flutterLocalNotificationsPlugin.initialize(initializationSettings);
+  }
+
+  Future<void> scheduleNotification(
+      tz.TZDateTime scheduledTime, String note) async {
+    const AndroidNotificationDetails androidPlatformChannelSpecifics =
+        AndroidNotificationDetails(
+      'your_channel_id',
+      'your_channel_name',
+      importance: Importance.max,
+      priority: Priority.high,
+    );
+    final NotificationDetails platformChannelSpecifics =
+        NotificationDetails(android: androidPlatformChannelSpecifics);
+
+    await flutterLocalNotificationsPlugin.zonedSchedule(
+      0,
+      'Hatırlatma',
+      note,
+      scheduledTime,
+      platformChannelSpecifics,
+      androidAllowWhileIdle: true,
+      uiLocalNotificationDateInterpretation:
+          UILocalNotificationDateInterpretation.absoluteTime,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Hatırlatıcı Notlar'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            TextField(
+              controller: _noteController,
+              decoration: InputDecoration(
+                hintText: 'Hatırlatma notu girin',
+              ),
+            ),
+            SizedBox(height: 16.0),
+            ElevatedButton(
+              onPressed: () async {
+                // Kullanıcının girdiği notu al
+                String note = _noteController.text;
+
+                // Hatırlatma zamanını 5 saniye sonrası olarak belirle
+                tz.initializeTimeZones();
+                var istanbul = tz.getLocation('Europe/Istanbul');
+                DateTime scheduledTime =
+                    DateTime.now().add(Duration(seconds: 5));
+
+                // Bildirimi planla
+                await scheduleNotification(
+                    tz.TZDateTime.from(scheduledTime, istanbul), note);
+
+                // Not alanını temizle
+                _noteController.clear();
+
+                // Kullanıcıya geri bildirim ver
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text('Hatırlatma 5 saniye sonra gösterilecek.'),
+                  ),
+                );
+              },
+              child: Text('Not Ekle'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _noteController.dispose();
+    super.dispose();
+  }
 }
